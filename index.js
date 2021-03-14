fb18329e1df0c02b06aac27cff671ef8abfd1583
SumeetShelar <56186357+SumeetShelar@users.noreply.github.com>
2021-03-15 00:50:55 +0530

    Added Special-price Section

--- index.js
+++ index.js
@@ -22,4 +22,16 @@ $(document).ready(function(){
             }
         }
     })
+
+    //Isotope Filter
+    var $grid = $(".grid").isotope({
+        itemSelector : '.grid-item',
+        layoutMode : 'fitRows'
+    });
+
+    //Filter items on Button click
+     $(".button-group").on("click","button",function(){
+         var filterValue = $(this).attr('data-filter');
+         $grid.isotope({filter: filterValue});
+     })
 });
