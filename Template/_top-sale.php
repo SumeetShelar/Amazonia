61733805078e9fa2aef6a153b8f6a51821b993ce
SumeetShelar <56186357+SumeetShelar@users.noreply.github.com>
2021-03-21 18:04:45 +0530

    Created product.php to fetch data from MySQL DB

--- Template/_top-sale.php
+++ Template/_top-sale.php
@@ -1,3 +1,8 @@
+<?PHP
+    $product_shuffle = $product->getData();
+?>
+
+
 <!--Top Sale-->
 <section id="top-sale">
 <div class="container py-5">
@@ -5,11 +10,12 @@
     <hr>
     <!--Owl Carousel-->
     <div class="owl-carousel owl-theme">
+    <?PHP foreach($product_shuffle as $item) { ?>
     <div class="item py-2">
         <div class="product font-rale">
-        <a href="#"><img src="./assets/products/1.png" alt="product1" class="img-fluid"></a>
+        <a href="#"><img src="<?PHP echo $item['item_image'] ?? "./assets/products/1.png"; ?>" alt="product1" class="img-fluid"></a>
         <div class="text-center">
-            <h6>Samsung Galaxy 10</h6>
+            <h6><?PHP echo $item['item_name'] ?? "Unknown"; ?></h6>
             <div class="rating text-warning font-size-12">
             <span><i class="fas fa-star"></i></span>
             <span><i class="fas fa-star"></i></span>
@@ -18,126 +24,13 @@
             <span><i class="far fa-star"></i></span>
             </div>
             <div class="price py-2">
-            <span>Rs 71,000</span>
+            <span>Rs<?PHP echo $item['item_price'] ?? '0'; ?></span>
             </div>
             <button type="submit" class="btn btn-warning font-size-12">Add to cart</button>
         </div>
         </div>
     </div>
-    <div class="item py-2">
-        <div class="product font-rale">
-        <a href="#"><img src="./assets/products/2.png" alt="product1" class="img-fluid"></a>
-        <div class="text-center">
-            <h6>Redmi Note 7</h6>
-            <div class="rating text-warning font-size-12">
-            <span><i class="fas fa-star"></i></span>
-            <span><i class="fas fa-star"></i></span>
-            <span><i class="fas fa-star"></i></span>
-            <span><i class="fas fa-star"></i></span>
-            <span><i class="far fa-star"></i></span>
-            </div>
-            <div class="price py-2">
-            <span>Rs 71,000</span>
-            </div>
-            <button type="submit" class="btn btn-warning font-size-12">Add to cart</button>
-        </div>
-        </div>
-    </div>
-    <div class="item py-2">
-        <div class="product font-rale">
-        <a href="#"><img src="./assets/products/3.png" alt="product1" class="img-fluid"></a>
-        <div class="text-center">
-            <h6>Samsung Galaxy 10</h6>
-            <div class="rating text-warning font-size-12">
-            <span><i class="fas fa-star"></i></span>
-            <span><i class="fas fa-star"></i></span>
-            <span><i class="fas fa-star"></i></span>
-            <span><i class="fas fa-star"></i></span>
-            <span><i class="far fa-star"></i></span>
-            </div>
-            <div class="price py-2">
-            <span>Rs 71,000</span>
-            </div>
-            <button type="submit" class="btn btn-warning font-size-12">Add to cart</button>
-        </div>
-        </div>
-    </div>
-    <div class="item py-2">
-        <div class="product font-rale">
-        <a href="#"><img src="./assets/products/4.png" alt="product1" class="img-fluid"></a>
-        <div class="text-center">
-            <h6>Samsung Galaxy 10</h6>
-            <div class="rating text-warning font-size-12">
-            <span><i class="fas fa-star"></i></span>
-            <span><i class="fas fa-star"></i></span>
-            <span><i class="fas fa-star"></i></span>
-            <span><i class="fas fa-star"></i></span>
-            <span><i class="far fa-star"></i></span>
-            </div>
-            <div class="price py-2">
-            <span>Rs 71,000</span>
-            </div>
-            <button type="submit" class="btn btn-warning font-size-12">Add to cart</button>
-        </div>
-        </div>
-    </div>
-    <div class="item py-2">
-        <div class="product font-rale">
-        <a href="#"><img src="./assets/products/5.png" alt="product1" class="img-fluid"></a>
-        <div class="text-center">
-            <h6>Samsung Galaxy 10</h6>
-            <div class="rating text-warning font-size-12">
-            <span><i class="fas fa-star"></i></span>
-            <span><i class="fas fa-star"></i></span>
-            <span><i class="fas fa-star"></i></span>
-            <span><i class="fas fa-star"></i></span>
-            <span><i class="far fa-star"></i></span>
-            </div>
-            <div class="price py-2">
-            <span>Rs 71,000</span>
-            </div>
-            <button type="submit" class="btn btn-warning font-size-12">Add to cart</button>
-        </div>
-        </div>
-    </div>
-    <div class="item py-2">
-        <div class="product font-rale">
-        <a href="#"><img src="./assets/products/6.png" alt="product1" class="img-fluid"></a>
-        <div class="text-center">
-            <h6>Samsung Galaxy 10</h6>
-            <div class="rating text-warning font-size-12">
-            <span><i class="fas fa-star"></i></span>
-            <span><i class="fas fa-star"></i></span>
-            <span><i class="fas fa-star"></i></span>
-            <span><i class="fas fa-star"></i></span>
-            <span><i class="far fa-star"></i></span>
-            </div>
-            <div class="price py-2">
-            <span>Rs 71,000</span>
-            </div>
-            <button type="submit" class="btn btn-warning font-size-12">Add to cart</button>
-        </div>
-        </div>
-    </div>
-    <div class="item py-2">
-        <div class="product font-rale">
-        <a href="#"><img src="./assets/products/11.png" alt="product1" class="img-fluid"></a>
-        <div class="text-center">
-            <h6>Samsung Galaxy 10</h6>
-            <div class="rating text-warning font-size-12">
-            <span><i class="fas fa-star"></i></span>
-            <span><i class="fas fa-star"></i></span>
-            <span><i class="fas fa-star"></i></span>
-            <span><i class="fas fa-star"></i></span>
-            <span><i class="far fa-star"></i></span>
-            </div>
-            <div class="price py-2">
-            <span>Rs 71,000</span>
-            </div>
-            <button type="submit" class="btn btn-warning font-size-12">Add to cart</button>
-        </div>
-        </div>
-    </div>
+    <?PHP } // closing foreach function ?>
     </div>
     <!--!Owl Carousel-->
 </div>
--- functions.php
+++ functions.php
@@ -3,7 +3,14 @@
 // require MySQL Connection
 require('database/DBController.php');
 
-//DBController Object
+// require Product class
+require('database/Product.php');
+
+// DBController Object
 $db = new DBController(); 
+
+// Product Object
+$product = new Product($db);
+
 
 ?>
