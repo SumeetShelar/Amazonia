d421b56fa4f3422b6df957b3013a184b3d3edf4d
SumeetShelar <56186357+SumeetShelar@users.noreply.github.com>
2021-03-21 17:00:49 +0530

    Added closing connection function

--- database/DBController.php
+++ database/DBController.php
@@ -18,6 +18,19 @@ class DBController {
             echo "Fail".$this->con->connect_error;
         }
     }
+
+    public function __destruct()
+    {
+        $this->closeConnection();    
+    }
+
+    // for mysqli Closing Connection
+    protected function closeConnection() {
+        if($this->con != null) {
+            $this->con->close();
+            $this->con = null;
+        }
+    }
 }
 
 ?>
