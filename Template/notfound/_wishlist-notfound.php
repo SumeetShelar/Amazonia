d2622bd7c8b5a8937baa22fe3480a054ccf5571b
SumeetShelar <56186357+SumeetShelar@users.noreply.github.com>
2021-03-25 13:26:25 +0530

    Minor Styling to both notfound pages

--- Template/notfound/_wishlist-notfound.php
+++ Template/notfound/_wishlist-notfound.php
@@ -6,8 +6,8 @@
         <div class="row">
             <div class="col-sm-9">
             <!--Empty Cart-->
-            <div class="row">
-                <div class="col-sm-12 border-top py-3 mt-3 text-center">
+            <div class="row border-top py-3 mt-3">
+                <div class="col-sm-12 text-center py-2">
                     <img src="./assets/blog/empty_cart.png" alt="Empty Wishlist" class="img-sluid" style="height: 200px;">
                     <p class="font-baloo font-size-16 text-black-50">Empty Wishlist</p>
                 </div>
